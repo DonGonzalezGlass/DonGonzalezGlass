8f272204680e749b9b41307807291ab050df9c78
Monica Gonzalez <mogon2990@gmail.com>
2018-08-27 21:26:17 -0400

    home page styled mroe and more css loaders added

--- client/components/Home.js
+++ client/components/Home.js
@@ -2,29 +2,30 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import './Home.css'
 
 const Home = props => {
   return (
-    <div>
+    <div className='home-container'>
       <picture>
         <source media='(min-width: 1200px)' srcSet='glass-mobile-min-1200.png' />
         <source media='(min-width: 1800px)' srcSet='glass-mobile-min-1800.png' />
         <source media='(min-width: 900px)' srcSet='glass-mobile-min-900.png' />
         <source media='(min-width: 600px)' srcSet='glass-mobile-min-600.png' />
         <source media='(max-width: 599px)' srcSet='glass-mobile-max-599.png' />
-        <img src='glass-mobile-min-1200.png' alt='Hand blown glass art mobile by Don Gonzalez' />
+        <img src='glass-mobile-min-1200.png' alt='Hand blown handmade glass art mobile by Don Gonzalez' />
       </picture>
-      <div>
-        <h4>Contemporary Art Glass designs by Don Gonzalez and his collection of 20th Century Glass</h4>
+      <div className='heading-container intro'>
+        <h3>Contemporary Art Glass designs by Don Gonzalez and his collection of 20th Century Glass</h3>
       </div>
-      <div>
-        <div>
-          <h5>Learn the art of glassblowing or glass casting with Don Gonzalez</h5>
-          <Link to='/workshops'><button>Workshops</button></Link>
+      <div className='heading-container action'>
+        <div className='action-row'>
+          <h4>Learn the art of glassblowing or glass casting</h4>
+          <Link to='/workshops'><button className='action-btn'>Workshops</button></Link>
         </div>
-        <div>
-          <h5>Handmade glass is one-of-a-kind</h5>
-          <Link to='/workshops'><button>Shop</button></Link>
+        <div className='action-row'>
+          <h4>Handmade glass is one-of-a-kind</h4>
+          <Link to='/workshops'><button className='action-btn'>Shop</button></Link>
         </div>
       </div>
     </div>
